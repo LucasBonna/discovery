5decf03587ff2d2c379e20dc94530539b0c45aaa
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-10-31 00:05:49 -0300

    first commit

---
+++ src/test/java/br/com/contafacil/bonnarotec/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contafacil.bonnarotec.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
